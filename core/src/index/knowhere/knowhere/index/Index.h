3854b262de1926cedd95da0830410ea22e2e29b1
sahuang <xiaohai.xu@zilliz.com>
2020-06-16 17:41:22 +0800

    Initial restructure of knowhere and add IVFFlat class
    Signed-off-by: sahuang <xiaohai.xu@zilliz.com>

--- core/src/index/knowhere/knowhere/index/Index.h
+++ core/src/index/knowhere/knowhere/index/Index.h
@@ -23,10 +23,13 @@ namespace knowhere {
 class Index : public milvus::cache::DataObj {
  public:
     virtual BinarySet
-    Serialize(const Config& config = Config()) = 0;
+    Serialize(const Config& config = Config());
 
     virtual void
-    Load(const BinarySet&) = 0;
+    Load(const BinarySet&);
+
+    virtual void
+    Load(const BinarySet&, const void*) {};
 };
 
 using IndexPtr = std::shared_ptr<Index>;
